4af5dcd2bd440d6008aaee7f676db3a2f7f2c762
OberonPrime117 <71881484+OberonPrime117@users.noreply.github.com>
2022-06-08 02:41:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/api',methods=['GET'])
 def hello_world():
-    d = {}
     a = str(request.args['Query'])
     result = app2(
         a,
         lang='en', # defaults to 'en'
         country='us' # defaults to 'us'
     )
-    d['Query'] = result['score']
-    return jsonify(d)
+    result.headers.add('Access-Control-Allow-Origin', '*')
+    value = int(result['score'])
+    return value
 
 if __name__ == '__main__':
     app.run()
